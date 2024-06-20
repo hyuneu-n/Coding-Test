0113b83de8243906c5f83556e904b92a8cd8c05f
JJERY <hyuneun@skuniv.ac.kr>
2024-06-20 23:16:17 +0900

    [Silver III] Title: 터렛, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -0,0 +1,22 @@
+import math
+
+T = int(input())
+
+for _ in range(T):
+    x1, y1, r1, x2, y2, r2 = list(map(int, input().split()))
+    
+    # 두 원의 중심 사이의 거리
+    dis = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    
+    if dis == 0:
+        if r1 == r2:
+            print(-1)
+        else:
+            print(0)
+    else:
+        if r1+r2 == dis or abs(r2-r1) == dis:
+            print(1)
+        elif ((abs(r1-r2) < dis) and (dis < r1+r2)):
+            print(2)
+        else:
+            print(0)
